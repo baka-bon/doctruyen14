d0ae449d6b87b32339e27b32992dbe6433bd35c3
baka-bon <193638775+baka-bon@users.noreply.github.com>
2025-12-09 20:45:20 +0700

    Update 09/12/2025 & localize og:image

--- git-ignore.sh
+++ git-ignore.sh
@@ -1,1 +1,2 @@
 find doctruyen14.vip/huong-sac-sen-hong-truyen-ll-cha-con -type f -exec git update-index --assume-unchanged {} \;
+git update-index --assume-unchanged doctruyen14.vip/cha-nuoi-va-con-gai/13/index.html
