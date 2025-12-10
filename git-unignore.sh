ffb7e00abab9d121987ff25f88f3058fe9d773bf
baka-bon <193638775+baka-bon@users.noreply.github.com>
2025-12-10 17:24:26 +0700

    Fix some pages no-new-line & add them to ignore list

---
+++ git-unignore.sh
@@ -0,0 +1,17 @@
+find doctruyen14.vip/huong-sac-sen-hong-truyen-ll-cha-con -type f -exec git update-index --no-assume-unchanged {} \;
+
+git update-index --no-assume-unchanged doctruyen14.vip/cha-nuoi-va-con-gai/13/index.html
+git update-index --no-assume-unchanged doctruyen14.vip/buoi-stream-thoa-man-cua-streamer/index.html
+git update-index --no-assume-unchanged doctruyen14.vip/chu-a-chau-xin-loi-truyen-co-that/13/index.html
+git update-index --no-assume-unchanged doctruyen14.vip/co-giao-no-le-humiliation-training-public/4/index.html
+git update-index --no-assume-unchanged doctruyen14.vip/co-tien-hang-xom/24/index.html
+git update-index --no-assume-unchanged doctruyen14.vip/con-gai-hao-sac/2/index.html
+git update-index --no-assume-unchanged doctruyen14.vip/ky-han-co-gai-den-tu-hom-qua-truyen-nguoi-lon-sieu-pham/4/index.html
+git update-index --no-assume-unchanged doctruyen14.vip/ky-niem-ems-da-tung-la-cua-anh-truyen-sex-lang-man-18/15/index.html
+git update-index --no-assume-unchanged doctruyen14.vip/ky-niem-ems-da-tung-la-cua-anh-truyen-sex-lang-man-18/index.html
+git update-index --no-assume-unchanged doctruyen14.vip/minh-la-vo-chong-ma/24/index.html
+git update-index --no-assume-unchanged doctruyen14.vip/nhung-mau-chuyen-ngan-ve-cuckold-cheating-loan-luan/18/index.html
+git update-index --no-assume-unchanged doctruyen14.vip/nhung-mau-chuyen-ngan-ve-cuckold-cheating-loan-luan/21/index.html
+git update-index --no-assume-unchanged doctruyen14.vip/phuot-cung-em-gai-loli-truyen-hay/49/index.html
+git update-index --no-assume-unchanged doctruyen14.vip/tai-sao-chung-ta-lai-ra-nong-noi-nay/2/index.html
+git update-index --no-assume-unchanged doctruyen14.vip/tu-truyẹn-toi-da-biet-den-sex-nhu-the-day-truyen-moi-2022/6/index.html
