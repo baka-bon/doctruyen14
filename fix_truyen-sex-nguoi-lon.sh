7499ceb429e1651a98a8bcaa9c399305c8d47755
baka-bon <193638775+baka-bon@users.noreply.github.com>
2025-12-10 19:27:51 +0700

    Update 10/12/2025

---
+++ fix_truyen-sex-nguoi-lon.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+find doctruyen14.vip/truyen-sex-nguoi-lon -type f -name "*.html" -exec perl -0777 -i -pe 's/\.\.\/truyen-sex-nguoi-lon\///mg' {} \;
--- git-ignore.sh
+++ git-ignore.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 find doctruyen14.vip/huong-sac-sen-hong-truyen-ll-cha-con -type f -exec git update-index --assume-unchanged {} \;
 
 git update-index --assume-unchanged doctruyen14.vip/cha-nuoi-va-con-gai/13/index.html
