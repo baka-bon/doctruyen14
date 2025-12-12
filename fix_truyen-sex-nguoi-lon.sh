5269e7d5d559784cec358268f882010d857d3903
baka-bon <193638775+baka-bon@users.noreply.github.com>
2025-12-12 23:51:10 +0700

    Update 12/12/2025 & exclude more links

--- fix_truyen-sex-nguoi-lon.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find doctruyen14.vip/truyen-sex-nguoi-lon -type f -name "*.html" -exec perl -0777 -i -pe 's/\.\.\/truyen-sex-nguoi-lon\///mg' {} \;
--- localize.sh
+++ localize.sh
@@ -8,4 +8,6 @@ find . -type f -name "*.html" -mindepth 4 -maxdepth 4 -exec perl -0777 -i -pe 's
 find . -type f -name "*.html" -mindepth 5 -maxdepth 5 -exec perl -0777 -i -pe 's/href="https:\/\/doctruyen14\.vip\/((?!feed\/|comments\/|wp-json\/|truyen-ma\/|truyen-cuoi\/|dang-truyen\/|nghe-audio-truyen-sex-mp3\/|truyen-gay\/|truyen-teen\/|truyen-tinh-cam\/|truyen-tranh\/|(?:tag\/)?(?:audio-truyen|truyen-audio))[^"]+\/(?<!feed\/))?"/href="..\/..\/..\/$1index.html"/mg' {} \;
 find . -type f -name "*.html" -mindepth 6 -maxdepth 6 -exec perl -0777 -i -pe 's/href="https:\/\/doctruyen14\.vip\/((?!feed\/|comments\/|wp-json\/|truyen-ma\/|truyen-cuoi\/|dang-truyen\/|nghe-audio-truyen-sex-mp3\/|truyen-gay\/|truyen-teen\/|truyen-tinh-cam\/|truyen-tranh\/|(?:tag\/)?(?:audio-truyen|truyen-audio))[^"]+\/(?<!feed\/))?"/href="..\/..\/..\/..\/$1index.html"/mg' {} \;
 
+find doctruyen14.vip/truyen-sex-nguoi-lon -type f -name "*.html" -exec perl -0777 -i -pe 's/\.\.\/truyen-sex-nguoi-lon\///mg' {} \;
+
 #find . -type f -name "*.html" -exec perl -0777 -i -pe 's/mage" content="(?:\.\.\/)*((?!http)[^"]+)"/mage" content="https:\/\/baka-bon.github.io\/doctruyen14\/href"/mg' {} \;
