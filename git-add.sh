f3df0b219e5356224ab323b0bf448dd1f5525e12
baka-bon <193638775+baka-bon@users.noreply.github.com>
2025-12-18 20:52:31 +0700

    Rename domain to .cc

--- git-add.sh
+++ git-add.sh
@@ -1,4 +1,4 @@
-git add doctruyen14.vip/truyen-sex-nguoi-lon
-git add doctruyen14.vip/truyen-les
-git add doctruyen14.vip/tac-gia
-git add doctruyen14.vip/tag
+git add doctruyen14.cc/truyen-sex-nguoi-lon
+git add doctruyen14.cc/truyen-les
+git add doctruyen14.cc/tac-gia
+git add doctruyen14.cc/tag
