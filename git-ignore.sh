e12a7ec075c63d800f21df676a666607aadc3321
baka-bon <193638775+baka-bon@users.noreply.github.com>
2025-12-21 03:41:25 +0700

    Fix nav menu of the rest of the files

--- git-ignore.sh
+++ git-ignore.sh
@@ -2,8 +2,8 @@
 
 find doctruyen14.cc/huong-sac-sen-hong-truyen-ll-cha-con -type f -exec git update-index --assume-unchanged {} \;
 
-git update-index --assume-unchanged doctruyen14.cc/cha-nuoi-va-con-gai/13/index.html
 git update-index --assume-unchanged doctruyen14.cc/buoi-stream-thoa-man-cua-streamer/index.html
+git update-index --assume-unchanged doctruyen14.cc/cha-nuoi-va-con-gai/13/index.html
 git update-index --assume-unchanged doctruyen14.cc/chu-a-chau-xin-loi-truyen-co-that/13/index.html
 git update-index --assume-unchanged doctruyen14.cc/co-giao-no-le-humiliation-training-public/4/index.html
 git update-index --assume-unchanged doctruyen14.cc/co-tien-hang-xom/24/index.html
