65366a9999839c7b9110ccfbfb86562580ef1c92
baka-bon <193638775+baka-bon@users.noreply.github.com>
2026-08-04 03:51:05 +0700

    Fix line-ending/paragraphs & update ignored list

--- git-unignore.sh
+++ git-unignore.sh
@@ -1,20 +1,55 @@
 #!/bin/bash
 
+# Missing pages
 find doctruyen14.biz/huong-sac-sen-hong-truyen-ll-cha-con -type f -exec git update-index --no-assume-unchanged {} \;
+find doctruyen14.biz/truyen-dem-khuya-20-ga-ban-trai-hao-sac -type f -exec git update-index --no-assume-unchanged {} \;
+
+# Wrong paragraph/new-line ending
+find doctruyen14.biz/con-gai-hao-sac -type f -exec git update-index --no-assume-unchanged {} \;
 
 git update-index --no-assume-unchanged doctruyen14.biz/buoi-stream-thoa-man-cua-streamer/index.html
 git update-index --no-assume-unchanged doctruyen14.biz/cha-nuoi-va-con-gai/13/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/cho-vo-di-du-dao-seri-truyen-sex-cuc-hay/22/index.html
 git update-index --no-assume-unchanged doctruyen14.biz/chu-a-chau-xin-loi-truyen-co-that/13/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/chu-a-chau-xin-loi-truyen-co-that/8/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/chu-a-chau-xin-loi-truyen-co-that/9/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/chup-len-nu-nhan-vien-ca-tinh-thich-pho-dam-trong-nha-ve-sinh-toi-da-de-doa-tong-tinh-khien-co-ay-tro-thanh-mon-do-choi-tinh-duc-cua-toi-mac-cho-toi-tieu-khien/9/index.html
 git update-index --no-assume-unchanged doctruyen14.biz/co-giao-no-le-humiliation-training-public/4/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/co-giao-van/16/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/co-giao-xinh-dep/2/index.html
 git update-index --no-assume-unchanged doctruyen14.biz/co-tien-hang-xom/24/index.html
-git update-index --no-assume-unchanged doctruyen14.biz/con-gai-hao-sac/2/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/em-nga-dang-sieu-mau-truyen-cuc-pham/15/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/hanh-trinh-tro-thanh-1-dam-nu-tu-truyen-cuc-dinh-2018/7/index.html
 git update-index --no-assume-unchanged doctruyen14.biz/ky-han-co-gai-den-tu-hom-qua-truyen-nguoi-lon-sieu-pham/4/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/ky-han-co-gai-den-tu-hom-qua-truyen-nguoi-lon-sieu-pham/6/index.html
 git update-index --no-assume-unchanged doctruyen14.biz/ky-niem-ems-da-tung-la-cua-anh-truyen-sex-lang-man-18/15/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/ky-niem-ems-da-tung-la-cua-anh-truyen-sex-lang-man-18/7/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/ky-niem-ems-da-tung-la-cua-anh-truyen-sex-lang-man-18/9/index.html
 git update-index --no-assume-unchanged doctruyen14.biz/ky-niem-ems-da-tung-la-cua-anh-truyen-sex-lang-man-18/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/minh-la-vo-chong-ma/13/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/minh-la-vo-chong-ma/14/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/minh-la-vo-chong-ma/15/index.html
 git update-index --no-assume-unchanged doctruyen14.biz/minh-la-vo-chong-ma/24/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/nguoi-di-xinh-dep-truyen-loan-luan-2021/13/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/nguoi-di-xinh-dep-truyen-loan-luan-2021/14/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/nguoi-di-xinh-dep-truyen-loan-luan-2021/18/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/nguoi-di-xinh-dep-truyen-loan-luan-2021/23/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/nguoi-di-xinh-dep-truyen-loan-luan-2021/26/index.html
 git update-index --no-assume-unchanged doctruyen14.biz/nhung-mau-chuyen-ngan-ve-cuckold-cheating-loan-luan/18/index.html
 git update-index --no-assume-unchanged doctruyen14.biz/nhung-mau-chuyen-ngan-ve-cuckold-cheating-loan-luan/21/index.html
 git update-index --no-assume-unchanged doctruyen14.biz/phuot-cung-em-gai-loli-truyen-hay/49/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/qua-khu-chua-ngu-yen-quang-doi-sinh-vien/11/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/qua-khu-chua-ngu-yen-quang-doi-sinh-vien/12/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/qua-khu-chua-ngu-yen-quang-doi-sinh-vien/13/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/tai-sao-chung-ta-lai-ra-nong-noi-nay/11/index.html
 git update-index --no-assume-unchanged doctruyen14.biz/tai-sao-chung-ta-lai-ra-nong-noi-nay/2/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/tieu-thu-trang-va-ong-tai-xe-may-man/4/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/toi/16/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/trai-nghiem-co-that-sex-den-nghien-dit/3/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/truyen-cuckold-cuc-manh/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/truyen-hoi-uc-doi-toi-truyen-ngan-co-that-100/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/truyen-thuyet-lesbos/2/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/truyen-thuyet-lesbos/5/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/truyen-thuyet-lesbos/7/index.html
+git update-index --no-assume-unchanged doctruyen14.biz/truyen-thuyet-lesbos/8/index.html
 git update-index --no-assume-unchanged doctruyen14.biz/tu-truyẹn-toi-da-biet-den-sex-nhu-the-day-truyen-moi-2022/6/index.html
-git update-index --no-assume-unchanged doctruyen14.biz/truyen-dem-khuya-20-ga-ban-trai-hao-sac/index.html
